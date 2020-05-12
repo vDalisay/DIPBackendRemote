a0d687efde36b149db23923b75e94a17c4566d62
I384733 <I384733@fhict.nl>
2020-05-12 14:54:23 +0200

    Push maven fix?

--- backend/src/main/java/REST/backend/BackendApplication.java
+++ backend/src/main/java/REST/backend/BackendApplication.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableOAuth2Sso
 @SpringBootApplication
 public class BackendApplication {
-
+ //Random comment
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
